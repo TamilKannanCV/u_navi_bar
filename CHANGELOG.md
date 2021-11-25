83433acc56fb70981cc03276000c7ef6f6631a64
TamilKannan-personal <tamilkk100@gmail.com>
2021-11-25 12:23:44 +0530

    color utils brightness changed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
 ## 0.0.1
 * Initial release.
+
+## 0.0.3
+* `ColorUtils` brightness changed from `MediaQuery.of(context).platformBrightness` to `Theme.of(context).brightness`
+
--- lib/utils/color_utils.dart
+++ lib/utils/color_utils.dart
@@ -7,7 +7,7 @@ class ColorUtils {
   static const Color kIconUnSelectedLight = Color(0xFF454747);
   static const Color kIconUnSelectedDark = Color(0xFFc5c7c6);
   Color get kIconUnSelected {
-    if (MediaQuery.of(context).platformBrightness == Brightness.dark) {
+    if (Theme.of(context).brightness == Brightness.dark) {
       return kIconUnSelectedDark;
     }
     return kIconUnSelectedLight;
@@ -16,7 +16,7 @@ class ColorUtils {
   static const Color kIconSelectedDark = Color(0xFFc2e6fe);
   static const Color kIconSelectedLight = Color(0xFF001d34);
   Color get kIconSelected {
-    if (MediaQuery.of(context).platformBrightness == Brightness.dark) {
+    if (Theme.of(context).brightness == Brightness.dark) {
       return kIconSelectedDark;
     }
     return kIconSelectedLight;
@@ -25,7 +25,7 @@ class ColorUtils {
   static const Color kBarBgDark = Color(0xFF2e2e32);
   static const Color kBarBgLight = Color(0xFFf1f5fa);
   Color get kBarBg {
-    if (MediaQuery.of(context).platformBrightness == Brightness.dark) {
+    if (Theme.of(context).brightness == Brightness.dark) {
       return kBarBgDark;
     }
     return kBarBgLight;
@@ -34,7 +34,7 @@ class ColorUtils {
   static const Color kIconBgLight = Color(0xFFc3e7ff);
   static const Color kIconBgDark = Color(0xFF014b77);
   Color get kIconBg {
-    if (MediaQuery.of(context).platformBrightness == Brightness.dark) {
+    if (Theme.of(context).brightness == Brightness.dark) {
       return kIconBgDark;
     }
     return kIconBgLight;
